PaimaStudios/midnight-game-2

commit af039f63970b6fced06decdf068c6decda2f15e9

Adds Loader scene (#11)
* Adds Loader scene

* loader-scene

* updates text

* Animates ellipses

* adds comment

* adds comment

* replaces bbcode

* cleanup

* starts adding loader to battle starting

* Renders loader on top of other scenes

* adds loader to finalize_quest()

* review suggestions

* Revert "adds loader to finalize_quest()"

This reverts commit 166b4888b416472a11d3142b715cf5f29996baa5.

* Revert "Revert "adds loader to finalize_quest()""

This reverts commit 090c8934ccadb22da88b1ce71c8daeb083d7b71c.

* Minor fix

* resets mockapi

* fixes battle_logic

* Fixes gameState undefined bug in ActiveBattle

phaser/src/battle/logic.ts
@@ -1,6 +1,5 @@
-import { call } from "@midnight-ntwrk/midnight-js-contracts";
 import { Game2DerivedState, safeJSONString } from "game2-api";
-import { Ability, BattleRewards, Effect, EFFECT_TYPE, Game2PrivateState, pureCircuits } from "game2-contract";
+import { Ability, BattleRewards, Effect, EFFECT_TYPE, pureCircuits } from "game2-contract";
 
 export type CombatCallbacks = {
     // triggered when an enemy blocks. enemy is the enemy that blocks

commit 85e7911b22e5d23549050d860ecb5d92f457f966

Battle fixes
* Fixes #17

* Fixes issue introduced in #11 when entering repeated battles

* Balances defense vs attack abilities

* Add projectile animations + allow AOE attacks to resolve at the same
  instead of sequentially, reducing animation time.

contract/src/generate.js
@@ -45,7 +45,7 @@ const gen_energy_player_block = () => abilities.map((a) => colors.map((c) => `((
 
 // enemy
 
-const gen_enemy_dmg = () => `const enemy_damage = (${max_enemies.map((enemy) => `battle.stats[${enemy}].attack`).join(' + ')}) as Uint<32>;`;
+const gen_enemy_dmg = () => `const enemy_damage = (${max_enemies.map((enemy) => `(battle.stats[${enemy}].attack * ((new_enemy_dmg_${enemy} > 0) as Uint<1>))`).join(' + ')}) as Uint<32>;`;
 
 const gen_enemy_block = () => max_enemies.map((enemy) => `const enemy_block_${enemy} = battle.stats[${enemy}].block as Uint<32>;`).join('\n    ');
 

contract/src/template.compact
@@ -81,7 +81,8 @@ pure circuit effect_score(effect: Maybe<Effect>): Uint<32> {
     const aoe = (1 + (effect.value.is_aoe as Uint<1>));
     const gen = ((effect.value.effect_type == EFFECT_TYPE.generate) as Uint<1>) * 3;
     const non_gen = ((effect.value.effect_type != EFFECT_TYPE.generate) as Uint<1>) * effect.value.amount;
-    return ((effect.is_some as Uint<1>) * ((aoe * non_gen) + gen)) as Uint<32>;
+    const attack_compensate = (1 + 4 * ((effect.value.effect_type == EFFECT_TYPE.attack_phys || effect.value.effect_type == EFFECT_TYPE.attack_ice || effect.value.effect_type == EFFECT_TYPE.attack_fire) as Uint<1>));
+    return ((effect.is_some as Uint<1>) * ((aoe * non_gen * attack_compensate) + gen)) as Uint<32>;
 }
 
 // starting 4 abilities
@@ -100,7 +101,7 @@ export pure circuit ability_base_block(): Ability {
     return Ability {
         some<Effect>(Effect {
             EFFECT_TYPE.block,
-            2,
+            10,
             false
         }),
         [none<Effect>(), none<Effect>(), none<Effect>()]
@@ -439,12 +440,19 @@ circuit combat_round_impl(battle_id: Field, abilities: Vector<3, Ability>, targe
     // See generate.js for an explanation of the code generation
 
     // CODE-GENERATION START
+    
+    INSERT_ENEMY_BLOCK_CODE_HERE
 
+    
     INSERT_PLAYER_BLOCK_CODE_HERE
 
     INSERT_PLAYER_DAMAGE_CODE_HERE
 
-    INSERT_ENEMY_BLOCK_CODE_HERE
+
+    const new_enemy_dmg_0 = enemy_block_0 < player_damage_0 ? (old_state.enemy_hp_0 + player_damage_0 - enemy_block_0) as Uint<32> : 0;
+    const new_enemy_dmg_1 = enemy_block_1 < player_damage_1 ? (old_state.enemy_hp_1 + player_damage_1 - enemy_block_1) as Uint<32> : 0;
+    const new_enemy_dmg_2 = enemy_block_2 < player_damage_2 ? (old_state.enemy_hp_2 + player_damage_2 - enemy_block_2) as Uint<32> : 0;
+
 
     INSERT_ENEMY_DAMAGE_CODE_HERE
 
@@ -455,9 +463,9 @@ circuit combat_round_impl(battle_id: Field, abilities: Vector<3, Ability>, targe
     return BattleState {
         INSERT_DECK_INDEX_BATTLE_STATE_INIT_CODE_HERE,
         player_block < enemy_damage ? (old_state.player_hp + enemy_damage - player_block) as Uint<32> : 0,
-        enemy_block_0 < player_damage_0 ? (old_state.enemy_hp_0 + player_damage_0 - enemy_block_0) as Uint<32> : 0,
-        enemy_block_1 < player_damage_1 ? (old_state.enemy_hp_1 + player_damage_1 - enemy_block_1) as Uint<32> : 0,
-        enemy_block_2 < player_damage_2 ? (old_state.enemy_hp_2 + player_damage_2 - enemy_block_2) as Uint<32> : 0
+        new_enemy_dmg_0,
+        new_enemy_dmg_1,
+        new_enemy_dmg_2
     };
 }
 

phaser/src/ability.ts
@@ -4,6 +4,11 @@
 import { Ability, Effect, EFFECT_TYPE } from "game2-contract";
 import { fontStyle } from "./main";
 
+/// Adjusts contract-level damage numbers to a base/average amount
+export function contractDamageToBaseUI(amount: number | bigint): number {
+    return Number(amount) * 5;
+}
+
 function addEffectIcons(container: Phaser.GameObjects.Container, effect: Effect, xOffset: number, yOffset: number): Phaser.GameObjects.GameObject[] {
     console.log(`addEffectIcons(${effect.effect_type}, ${effect.amount})`);
     let uiComponents = [];
@@ -13,15 +18,15 @@ function addEffectIcons(container: Phaser.GameObjects.Container, effect: Effect,
     switch (effect.effect_type) {
         case EFFECT_TYPE.attack_fire:
             uiComponents.push(container.scene.add.image(xOffset + 8, yOffset, 'fire'));
-            uiComponents.push(container.scene.add.text(xOffset - 2, yOffset - 3, effect.amount.toString(), fontStyle(8)).setOrigin(0.5, 0.5));
+            uiComponents.push(container.scene.add.text(xOffset - 2, yOffset - 3, contractDamageToBaseUI(effect.amount).toString(), fontStyle(8)).setOrigin(0.5, 0.5));
             break;
         case EFFECT_TYPE.attack_ice:
             uiComponents.push(container.scene.add.image(xOffset + 8, yOffset, 'ice'));
-            uiComponents.push(container.scene.add.text(xOffset - 2, yOffset - 3, effect.amount.toString(), fontStyle(8)).setOrigin(0.5, 0.5));
+            uiComponents.push(container.scene.add.text(xOffset - 2, yOffset - 3, contractDamageToBaseUI(effect.amount).toString(), fontStyle(8)).setOrigin(0.5, 0.5));
             break;
         case EFFECT_TYPE.attack_phys:
             uiComponents.push(container.scene.add.image(xOffset + 8, yOffset, 'physical'));
-            uiComponents.push(container.scene.add.text(xOffset - 2, yOffset - 3, effect.amount.toString(), fontStyle(8)).setOrigin(0.5, 0.5));
+            uiComponents.push(container.scene.add.text(xOffset - 2, yOffset - 3, contractDamageToBaseUI(effect.amount).toString(), fontStyle(8)).setOrigin(0.5, 0.5));
             break;
         case EFFECT_TYPE.block:
             uiComponents.push(container.scene.add.image(xOffset + 8, yOffset, 'block'));

phaser/src/battle/logic.ts
@@ -8,7 +8,7 @@ export type CombatCallbacks = {
     onEnemyAttack: (enemy: number, amount: number) => Promise<void>;
     // triggered when a player's ability causes an effect (directly or via trigger)
     // reminder: `amount` is the color for EFFECT_TYPE.generate (range [0, 2])
-    onPlayerEffect: (target: number, effectType: EFFECT_TYPE, amount: number) => Promise<void>;
+    onPlayerEffect: (targets: number[], effectType: EFFECT_TYPE, amounts: number[]) => Promise<void>;
     // triggered at the start of a round to show which abilities are being played this round
     onDrawAbilities: (abilities: Ability[]) => Promise<void>;
     // triggered when an ability is used. energy == undefined means base effect applying, otherwise it specifies which trigger is being applied
@@ -49,42 +49,85 @@ export function combat_round_logic(battle_id: bigint, gameState: Game2DerivedSta
         const abilities = battleState.deck_indices.map((i) => battleConfig.loadout.abilities[Number(i)]);
 
         let player_block = BigInt(0);
-        let player_damage = [BigInt(0), BigInt(0), BigInt(0)];
+        const enemy_count = Number(battleConfig.enemy_count);
+        let player_damage = new Array(enemy_count).fill(BigInt(0));
         let energy = [false, false, false];
-
-        // enemy damage
         let enemy_damage = BigInt(0);
-        const enemy_hp = [battleState.enemy_hp_0, battleState.enemy_hp_1, battleState.enemy_hp_2];
+
+        // to be able to early-abort we have this in a lambda
+        const handleEndOfRound = () => {
+            console.log(`${uiHooks == undefined} handleEndOfRound.player_damage = ${player_damage}`);
+            if (enemy_damage > player_block) {
+                battleState.player_hp -= enemy_damage - player_block;
+            }
+            if (player_damage[0] > battleConfig.stats[0].block) {
+                battleState.enemy_hp_0 = BigInt(Math.max(0, Number(battleState.enemy_hp_0 + battleConfig.stats[0].block - player_damage[0])));
+            }
+            if (player_damage[1] > battleConfig.stats[1].block) {
+                battleState.enemy_hp_1 = BigInt(Math.max(0, Number(battleState.enemy_hp_1 + battleConfig.stats[1].block - player_damage[1])));
+            }
+            if (player_damage[2] > battleConfig.stats[2].block) {
+                battleState.enemy_hp_2 = BigInt(Math.max(0, Number(battleState.enemy_hp_2 + battleConfig.stats[2].block - player_damage[2])));
+            }
+            console.log(`Player HP ${battleState.player_hp} | Enemy HP: ${battleState.enemy_hp_0} / ${battleState.enemy_hp_1} / ${battleState.enemy_hp_2}`);
+            if (battleState.player_hp <= 0) {
+                console.log(`YOU DIED`);
+                resolve({ alive: false, gold: BigInt(0), ability: { is_some: false, value: BigInt(0) } });
+            }
+            else if (battleState.enemy_hp_0 <= 0 && battleState.enemy_hp_1 <= 0 && battleState.enemy_hp_2 <= 0) {
+                console.log(`YOU WON`);
+                // TODO how to determine rewards?
+                resolve({ alive: true, gold: BigInt(randIntBetween(rng, 1000, 50, 200)), ability: { is_some: false, value: BigInt(0) } });
+            } else {
+                console.log(`CONTINUE BATTLE`);
+                resolve(undefined);
+            }
+            console.log(`end state[${uiHooks == undefined}]: ${safeJSONString(gameState)}`);
+        }
+
+
+
+        // enemy block
+        let enemy_hp = [battleState.enemy_hp_0, battleState.enemy_hp_1, battleState.enemy_hp_2];
         for (let i = 0; i < battleConfig.enemy_count; ++i) {
             if (enemy_hp[i] > 0) {
                 // do not change vars for block since it's directly checked during player against enemy damage code
                 const block = Number(battleConfig.stats[i].block);
+                console.log(`${uiHooks == undefined}, yes block ${i}`);
                 if (block != 0) {
                     await uiHooks?.onEnemyBlock(i, block);
                 }
+            } else {
+                console.log(`${uiHooks == undefined} skipping block ${i}`);
             }
         }
 
+
+
+        // player abilities (all)
         const resolveEffect = async (effect: { is_some: boolean, value: Effect }, target: number) => {
-            for (let enemy = 0; enemy < 3; ++enemy) {
-                if (effect.is_some && (effect.value.is_aoe || target == enemy)) {
-                    switch (effect.value.effect_type) {
-                        case EFFECT_TYPE.attack_fire:
-                        case EFFECT_TYPE.attack_ice:
-                        case EFFECT_TYPE.attack_phys:
+            if (effect.is_some) {
+                const targets = effect.value.is_aoe ? new Array(Number(battleConfig.enemy_count)).fill(0).map((_, i) => i) : [target];
+                switch (effect.value.effect_type) {
+                    case EFFECT_TYPE.attack_fire:
+                    case EFFECT_TYPE.attack_ice:
+                    case EFFECT_TYPE.attack_phys:
+                        const amounts = targets.map((enemy) => {
                             const dmg = pureCircuits.effect_damage(effect.value, battleConfig.stats[enemy]);
-                            await uiHooks?.onPlayerEffect(enemy, effect.value.effect_type, Number(dmg));
                             player_damage[enemy] += dmg;
-                            break;
-                        case EFFECT_TYPE.block:
-                            await uiHooks?.onPlayerEffect(enemy, effect.value.effect_type, Number(effect.value.amount));
-                            player_block += effect.value.amount;
-                            break;
-                        case EFFECT_TYPE.generate:
-                            await uiHooks?.onPlayerEffect(enemy, effect.value.effect_type, Number(effect.value.amount));
-                            energy[Number(effect.value.amount)] = true;
-                            break;
-                    }
+                            console.log(`[${uiHooks == undefined}] player_damage[${enemy}] = ${player_damage[enemy]} // took ${dmg} damage`);
+                            return Number(dmg)
+                        });
+                        await uiHooks?.onPlayerEffect(targets, effect.value.effect_type, amounts);
+                        break;
+                    case EFFECT_TYPE.block:
+                        await uiHooks?.onPlayerEffect(targets, effect.value.effect_type, [Number(effect.value.amount)]);
+                        player_block += effect.value.amount;
+                        break;
+                    case EFFECT_TYPE.generate:
+                        await uiHooks?.onPlayerEffect(targets, effect.value.effect_type, [Number(effect.value.amount)]);
+                        energy[Number(effect.value.amount)] = true;
+                        break;
                 }
             }
         };
@@ -93,13 +136,25 @@ export function combat_round_logic(battle_id: bigint, gameState: Game2DerivedSta
 
         // TODO: don't target dead enemies
         const targets = abilities.map((_, i) => randIntBetween(rng, i, 0, Number(battleConfig.enemy_count) - 1));
-        // TODO: when you have internet check if you can do this with forEach but chaining promises one after the other
+        
+        // base effects
+        const allEnemiesDead = () => {
+            console.log(`[${uiHooks == undefined}] checking damage: ${player_damage} | blocks: ${battleConfig.stats[0].block}, ${battleConfig.stats[1].block}, ${battleConfig.stats[2].block}   |  hp: ${battleState.enemy_hp_0}, ${battleState.enemy_hp_1}, ${battleState.enemy_hp_2}`);
+            return (player_damage[0] > battleConfig.stats[0].block + battleState.enemy_hp_0)
+                                  && (player_damage[1] > battleConfig.stats[1].block + battleState.enemy_hp_1 || enemy_count < 2)
+                                  && (player_damage[2] > battleConfig.stats[2].block + battleState.enemy_hp_2 || enemy_count < 3);
+        };
         for (let i = 0; i < abilities.length; ++i) {
             const ability = gameState.allAbilities.get(abilities[i])!;
             await uiHooks?.onUseAbility(i, undefined);
             await resolveEffect(ability.effect, targets[i]);
             await uiHooks?.afterUseAbility(i);
+            if (allEnemiesDead()) {
+                console.log(`[${uiHooks == undefined}] prematurely ending`);
+                return handleEndOfRound();
+            }
         }
+        // energy triggers
         for (let i = 0; i < 3; ++i) {
             if (energy[i]) {
                 if (abilities.some((id) => gameState.allAbilities.get(id)?.on_energy[i].is_some)) {
@@ -110,6 +165,9 @@ export function combat_round_logic(battle_id: bigint, gameState: Game2DerivedSta
                             await uiHooks?.onUseAbility(j, i);
                             await resolveEffect(ability.on_energy[i], targets[j]);
                             await uiHooks?.afterUseAbility(j);
+                            if (allEnemiesDead()) {
+                                return handleEndOfRound();
+                            }
                         }
                     }
                 }
@@ -123,6 +181,8 @@ export function combat_round_logic(battle_id: bigint, gameState: Game2DerivedSta
         //     }
         // });
 
+
+
         // enemy damage
         for (let i = 0; i < battleConfig.enemy_count; ++i) {
             if (enemy_hp[i] > 0) {
@@ -134,31 +194,6 @@ export function combat_round_logic(battle_id: bigint, gameState: Game2DerivedSta
             }
         }
 
-        if (enemy_damage > player_block) {
-            battleState.player_hp -= enemy_damage - player_block;
-        }
-        if (player_damage[0] > battleConfig.stats[0].block) {
-            battleState.enemy_hp_0 = BigInt(Math.max(0, Number(battleState.enemy_hp_0 + battleConfig.stats[0].block - player_damage[0])));
-        }
-        if (player_damage[1] > battleConfig.stats[1].block) {
-            battleState.enemy_hp_1 = BigInt(Math.max(0, Number(battleState.enemy_hp_1 + battleConfig.stats[1].block - player_damage[1])));
-        }
-        if (player_damage[2] > battleConfig.stats[2].block) {
-            battleState.enemy_hp_2 = BigInt(Math.max(0, Number(battleState.enemy_hp_2 + battleConfig.stats[2].block - player_damage[2])));
-        }
-        console.log(`Player HP ${battleState.player_hp} | Enemy HP: ${battleState.enemy_hp_0} / ${battleState.enemy_hp_1} / ${battleState.enemy_hp_2}`);
-        if (battleState.player_hp <= 0) {
-            console.log(`YOU DIED`);
-            resolve({ alive: false, gold: BigInt(0), ability: { is_some: false, value: BigInt(0) } });
-        }
-        else if (battleState.enemy_hp_0 <= 0 && battleState.enemy_hp_1 <= 0 && battleState.enemy_hp_2 <= 0) {
-            console.log(`YOU WON`);
-            // TODO how to determine rewards?
-            resolve({ alive: true, gold: BigInt(randIntBetween(rng, 1000, 50, 200)), ability: { is_some: false, value: BigInt(0) } });
-        } else {
-            console.log(`CONTINUE BATTLE`);
-            resolve(undefined);
-        }
-        console.log(`end state[${uiHooks == undefined}]: ${safeJSONString(gameState)}`);
+        handleEndOfRound();
     });
 }

phaser/src/menus/battle.ts
@@ -50,6 +50,8 @@ export class ActiveBattle extends Phaser.Scene {
             // we will need that for combining multiple rounds if we get proof composition in time
             let apiDone = false;
             let loaderStarted = false;
+
+            const clonedState = structuredClone(this.state!);
             
             const apiPromise = this.api.combat_round(id).then(result => {
                 apiDone = true;
@@ -59,30 +61,67 @@ export class ActiveBattle extends Phaser.Scene {
                 return result;
             });
             
-            const uiPromise = combat_round_logic(id, this.state!, {
+            const uiPromise = combat_round_logic(id, clonedState, {
                 onEnemyBlock: (enemy: number, amount: number) => new Promise((resolve) => {
                     this.enemies[enemy].addBlock(amount);
+                    //console.log(`enemy [${amount}] blocked for ${}`);
                     this.add.existing(new BattleEffect(this, enemyX(this.battle, enemy), enemyY() - 32, EFFECT_TYPE.block, amount, resolve));
                 }),
                 onEnemyAttack: (enemy: number, amount: number) => new Promise((resolve) => {
-                    this.player?.damage(amount);
-                    this.add.existing(new BattleEffect(this, enemyX(this.battle, enemy), enemyY() - 32, EFFECT_TYPE.attack_phys, amount, resolve));
+                    const fist = this.add.image(enemyX(this.battle, enemy), enemyY(), 'physical');
+                    this.tweens.add({
+                        targets: fist,
+                        x: playerX(),
+                        y: playerY(),
+                        duration: 100,
+                        onComplete: () => {
+                            fist.destroy();
+                            this.player?.damage(amount);
+                            this.add.existing(new BattleEffect(this, playerX(), playerY() - 32, EFFECT_TYPE.attack_phys, amount, resolve));
+                        }
+                    });
                 }),
-                onPlayerEffect: (target: number, effectType: EFFECT_TYPE, amount: number) => new Promise((resolve) => {
+                onPlayerEffect: (targets: number[], effectType: EFFECT_TYPE, amounts: number[]) => new Promise((resolve) => {
+                    console.log(`onPlayerEffect(${targets}, ${effectType}, ${amounts})`);
+                    let damageType = undefined;
                     switch (effectType) {
                         case EFFECT_TYPE.attack_fire:
+                            damageType = 'fire';
+                            break;
                         case EFFECT_TYPE.attack_ice:
+                            damageType = 'ice';
+                            break;
                         case EFFECT_TYPE.attack_phys:
-                            this.enemies[target].damage(amount);
+                            damageType = 'physical';
                             break;
                         case EFFECT_TYPE.block:
-                            this.player?.addBlock(amount);
+                            this.player?.addBlock(amounts[0]);
                             break;
                         case EFFECT_TYPE.generate:
                             // TODO
                             break;
                     }
-                    this.add.existing(new BattleEffect(this, playerX(), playerY() - 32, effectType, amount, resolve));
+                    if (damageType != undefined) {
+                        for (let i = 0; i < targets.length; ++i) {
+                            const target = targets[i];
+                            const amount = amounts[i];
+                            const bullet = this.add.image(playerX(), playerY(), damageType);
+                            this.tweens.add({
+                                targets: bullet,
+                                x: enemyX(this.battle, target),
+                                y: enemyY(),
+                                duration: 150,
+                                onComplete: () => {
+                                    //console.log(`enemy ${target} took ${effect.amount} damage`);
+                                    this.enemies[target].damage(amount);
+                                    this.add.existing(new BattleEffect(this, bullet.x, bullet.y - 32, effectType, amount, resolve));
+                                    bullet.destroy();
+                                },
+                            });
+                        }
+                    } else {
+                        this.add.existing(new BattleEffect(this, playerX(), playerY() - 32, effectType, amounts[0], resolve));
+                    }
                 }),
                 onDrawAbilities: (abilities: Ability[]) => new Promise((resolve) => {
                     this.abilityIcons = abilities.map((ability, i) => new AbilityWidget(this, GAME_WIDTH * (i + 0.5) / abilities.length, abilityIdleY(), ability).setAlpha(0));
@@ -96,7 +135,7 @@ export class ActiveBattle extends Phaser.Scene {
                     });
                 }),
                 onUseAbility: (abilityIndex: number, energy?: number) => new Promise((resolve) => {
-                    const abilityIcons = this.abilityIcons[abilityIndex];
+                    const abilityIcons = this.abilityIcons[abilityIndex]
                     this.tweens.add({
                         targets: abilityIcons,
                         y: abilityInUseY(),
@@ -104,7 +143,7 @@ export class ActiveBattle extends Phaser.Scene {
                         duration: 250,
                         onComplete: () => {
                             this.tweens.add({
-                                targets: energy !== undefined ? abilityIcons.energyEffectUI[energy] : abilityIcons.baseEffectUI,
+                                targets: energy != undefined ? abilityIcons.energyEffectUI[energy] : abilityIcons.baseEffectUI,
                                 scale: 1.5,
                                 yoyo: true,
                                 delay: 100,

phaser/src/menus/pre-battle.ts
@@ -92,7 +92,7 @@ export class StartBattleMenu extends Phaser.Scene {
                         }
                         this.events.on('stateChange', () => {
                             this.scene.stop('Loader');
-                            this.scene.remove('ActiveBattloaderle');
+                            this.scene.remove('ActiveBattle');
                             this.scene.add('ActiveBattle', new ActiveBattle(this.api, battle, this.state));
                             this.scene.start('ActiveBattle');
                         });

phaser/src/mockapi.ts
@@ -170,22 +170,37 @@ export class MockGame2API implements DeployedGame2API {
         const randomEffect = (factor: number, canBeGenerate: boolean) => {
             const effectType = Phaser.Math.Between(0, canBeGenerate ? 4 : 3) as EFFECT_TYPE;
             const aoe = effectType != EFFECT_TYPE.block && effectType != EFFECT_TYPE.generate ? Math.random() > 0.7 : false;
+            let amount = -1;
+            switch (effectType) {
+                case EFFECT_TYPE.attack_fire:
+                case EFFECT_TYPE.attack_ice:
+                case EFFECT_TYPE.attack_phys:
+                    amount = Phaser.Math.Between(factor * Number(difficulty), 2 * factor * Number(difficulty));
+                    break;
+                case EFFECT_TYPE.block:
+                    amount = 5 * Phaser.Math.Between(factor * Number(difficulty), 2 * factor * Number(difficulty));
+                    break;
+                case EFFECT_TYPE.generate:
+                    amount = Phaser.Math.Between(0, 2);
+                    break;
+            }
             return {
                 is_some: true,
                 value: {
                     effect_type: effectType,
-                    amount: BigInt(effectType == EFFECT_TYPE.generate ? Phaser.Math.Between(0, 2) : Phaser.Math.Between(factor * Number(difficulty), 2 * factor * Number(difficulty))),
+                    amount: BigInt(amount),
                     is_aoe: aoe
                 }
             };
         };
         const triggers = [nullEffect, nullEffect, nullEffect];
         const color = Phaser.Math.Between(0, 5);
+        const baseEffect = randomEffect(color < triggers.length ? 1 : 2, true);
         if (color < triggers.length) {
             // triggers[0] = randomEffect(2);
             // triggers[1] = randomEffect(2);
             // triggers[2] = randomEffect(2);
-            if (Math.random() > 0.7) {
+            if (Math.random() > 0.7 || (baseEffect.value.effect_type == EFFECT_TYPE.generate && baseEffect.value.amount == BigInt(color))) {
                 for (let i = 0; i < 3; ++i) {
                     if (i != color) {
                         triggers[i] = randomEffect(1, false);
@@ -196,7 +211,7 @@ export class MockGame2API implements DeployedGame2API {
             }
         }
         const ability = {
-            effect: randomEffect(color < triggers.length ? 1 : 2, true),
+            effect: baseEffect,
             on_energy: triggers,
         };
         const abilityId = pureCircuits.derive_ability_id(ability);