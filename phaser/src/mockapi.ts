cb0ec02fc91847fd30f2a7852f3e9842391978b1
JOSHUA QUIRK <ap@mescl.in>
2025-08-21 13:05:18 -0400

    refactor

--- phaser/src/mockapi.ts
+++ phaser/src/mockapi.ts
@@ -135,38 +135,11 @@ export class MockGame2API implements DeployedGame2API {
 
     public async combat_round(battle_id: bigint, targets?: [bigint, bigint, bigint]): Promise<BattleRewards | undefined> {
         return await this.response(async () => {
-            if (targets) {
-                const targetsCopy = targets.map(t => Number(t));
-                const { combat_round_logic } = await import('./battle/logic');
-                return combat_round_logic(battle_id, this.mockState, targetsCopy).then((ret) => {
-                    const battleState = this.mockState.activeBattleStates.get(battle_id)!;
-                    // shift deck current abilities
-                    const DECK_SIZE = 7;
-                    const OFFSETS = [1, 2, 3];
-                    for (let i = 0; i < battleState.deck_indices.length; ++i) {
-                        battleState.deck_indices[i] = BigInt((Number(battleState.deck_indices[i]) + OFFSETS[i]) % DECK_SIZE);
-                        for (let j = 0; j < i; ++j) {
-                            if (battleState.deck_indices[i] == battleState.deck_indices[j]) {
-                                battleState.deck_indices[i] = BigInt((Number(battleState.deck_indices[i]) + 1) % DECK_SIZE);
-                            }
-                            for ( let k = 0; k < j; ++k) {
-                                if (battleState.deck_indices[i] == battleState.deck_indices[k]) {
-                                    battleState.deck_indices[i] = BigInt((Number(battleState.deck_indices[i]) + 1) % DECK_SIZE);
-                                }
-                            }
-                        }
-                    }
-                    if (ret != undefined) {
-                        this.addRewards(ret);
-                        this.mockState.activeBattleConfigs.delete(battle_id);
-                        this.mockState.activeBattleStates.delete(battle_id);
-                    }
-                    return ret;
-                });
-            } else {
-                return combat_round_logic(battle_id, this.mockState, undefined).then((ret) => {
+            const targetsUnwrapped = targets?.map(t => Number(t));
+
+            return combat_round_logic(battle_id, this.mockState, targetsUnwrapped).then((ret) => {
                 const battleState = this.mockState.activeBattleStates.get(battle_id)!;
-                // shift deck current abilities
+                // Shift deck current abilities
                 const DECK_SIZE = 7;
                 const OFFSETS = [1, 2, 3];
                 for (let i = 0; i < battleState.deck_indices.length; ++i) {
@@ -175,7 +148,7 @@ export class MockGame2API implements DeployedGame2API {
                         if (battleState.deck_indices[i] == battleState.deck_indices[j]) {
                             battleState.deck_indices[i] = BigInt((Number(battleState.deck_indices[i]) + 1) % DECK_SIZE);
                         }
-                        for ( let k = 0; k < j; ++k) {
+                        for (let k = 0; k < j; ++k) {
                             if (battleState.deck_indices[i] == battleState.deck_indices[k]) {
                                 battleState.deck_indices[i] = BigInt((Number(battleState.deck_indices[i]) + 1) % DECK_SIZE);
                             }
@@ -189,7 +162,6 @@ export class MockGame2API implements DeployedGame2API {
                 }
                 return ret;
             });
-            }
         });
     }
 
