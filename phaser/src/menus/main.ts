872a583d98f9ebb02f3547e6c80dd937d5032d24
Joshua Quirk <ap@mescl.in>
2025-11-07 13:35:40 -0500

    Refactors preload into boot.ts (#159)
    * Adds retreat functionality to battles
    
    * fixes coyote sizing
    
    * Fixes retreat bugs
    
    * fixes bug
    
    * fixes bug
    
    * cleanup
    
    * Refactors code out to boot.ts
    
    * refactor

--- phaser/src/menus/main.ts
+++ phaser/src/menus/main.ts
@@ -9,8 +9,6 @@ import { Loader } from "./loader";
 import { Subscription } from "rxjs";
 import { fontStyle, GAME_HEIGHT, GAME_WIDTH, logger } from "../main";
 import { ShopMenu } from "./shop/shop";
-import { createSpiritAnimations } from "../animations/spirit";
-import { createEnemyAnimations } from "../animations/enemy";
 import { BiomeSelectMenu } from "./biome-select";
 import { QuestsMenu } from "./quests";
 import { DungeonScene } from "./dungeon-scene";
@@ -35,103 +33,6 @@ export class MainMenu extends Phaser.Scene {
         }, 100);
     }
 
-    preload() {
-        this.load.setBaseURL('/');
-
-        // UI Sprites
-        this.load.image('ui-scroll-bg', 'ui-scroll-bg.png');
-        this.load.image('tablet0', 'tablet0.png');
-        this.load.image('tablet1', 'tablet1.png');
-        this.load.image('tablet2', 'tablet2.png');
-        this.load.image('tablet-round', 'tablet-round.png');
-        this.load.image('lock-icon', 'lock-icon.png');
-
-        // Icon sprites
-        this.load.image('fire', 'fire.png');
-        this.load.image('ice', 'ice.png');
-        this.load.image('physical', 'physical.png');
-        this.load.image('block', 'block.png');
-        this.load.image('heal', 'heal.png');
-        this.load.image('energy-icon', 'energy-icon.png');
-        this.load.image('arrow', 'arrow.png');
-        this.load.image('aoe', 'aoe.png');
-        this.load.image('hp-bar-shield', 'hp-bar-shield.png');
-        this.load.image('upgrade-star', 'upgrade-star.png');
-        this.load.image('upgrade-star-slot', 'upgrade-star-slot.png');
-        this.load.image('upgrade-star-background', 'upgrade-star-background.png');
-
-        // Revolving Orb Sprites
-        this.load.image('orb-atk-fire', 'orb-atk-fire.png');
-        this.load.image('orb-atk-ice', 'orb-atk-ice.png');
-        this.load.image('orb-atk-phys', 'orb-atk-phys.png');
-        this.load.image('orb-def', 'orb-def.png');
-
-        // Spirit Sprites
-        this.load.spritesheet('spirit-atk-fire', 'spirit-atk-fire.png', { frameWidth: 64, frameHeight: 64 });
-        this.load.spritesheet('spirit-atk-ice', 'spirit-atk-ice.png', { frameWidth: 64, frameHeight: 64 });
-        this.load.spritesheet('spirit-atk-phys', 'spirit-atk-phys.png', { frameWidth: 64, frameHeight: 64 });
-        this.load.spritesheet('spirit-def', 'spirit-def.png', { frameWidth: 64, frameHeight: 64 });
-
-        this.load.spritesheet('orb-aura', 'orb-aura.png', { frameWidth: 16, frameHeight: 16 });
-        this.load.spritesheet('spirit-aura', 'spirit-aura.png', { frameWidth: 32, frameHeight: 32 });
-
-        // Enemy Sprites
-        this.load.spritesheet('enemy-goblin', 'enemy-goblin.png', { frameWidth: 32, frameHeight: 28 });
-        this.load.spritesheet('enemy-hellspawn', 'enemy-hellspawn.png', { frameWidth: 61, frameHeight: 47 });
-        this.load.spritesheet('enemy-fire-sprite', 'enemy-fire-sprite.png', { frameWidth: 43, frameHeight: 35 });
-        this.load.spritesheet('enemy-ice-golem', 'enemy-ice-golem.png', { frameWidth: 44, frameHeight: 40 });
-        this.load.spritesheet('enemy-snowman', 'enemy-snowman.png', { frameWidth: 40, frameHeight: 40 });
-        this.load.spritesheet('enemy-coyote', 'enemy-coyote.png', { frameWidth: 60, frameHeight: 35 });
-        this.load.spritesheet('enemy-pyramid', 'enemy-pyramid.png', { frameWidth: 80, frameHeight: 80 });
-        this.load.spritesheet('enemy-goblin-priest', 'enemy-goblin-priest.png', { frameWidth: 32, frameHeight: 32 });
-	    this.load.spritesheet('enemy-goblin-swordmaster', 'enemy-goblin-swordmaster.png', { frameWidth: 47, frameHeight: 32  });
-        this.load.spritesheet('enemy-tentacle', 'enemy-tentacle.png', { frameWidth: 18, frameHeight: 57  });
-        this.load.spritesheet('enemy-miniboss-goblin-chief', 'enemy-miniboss-goblin-chief.png', { frameWidth: 64, frameHeight: 56  });
-        this.load.spritesheet('enemy-miniboss-tentacles', 'enemy-miniboss-tentacles.png', { frameWidth: 64, frameHeight: 64 });
-
-        this.load.spritesheet('enemy-boss-enigma', 'enemy-boss-enigma-1.png', { frameWidth: 152, frameHeight: 95 });
-        this.load.spritesheet('enemy-boss-dragon', 'enemy-boss-dragon-1.png', { frameWidth: 145, frameHeight: 97 });
-        this.load.spritesheet('enemy-boss-abominable', 'enemy-boss-abominable.png', { frameWidth: 130, frameHeight: 98 });
-        this.load.spritesheet('enemy-boss-sphinx', 'enemy-boss-sphinx.png', { frameWidth: 80, frameHeight: 94 });
-
-        // Combat Effects
-        this.load.image('heal-effect-circle', 'heal-effect-circle.png');
-        this.load.image('heal-effect-rays', 'heal-effect-rays.png');
-
-        // Backgrounds
-        this.load.image('bg-hub1', 'bg-hub1.png');
-        this.load.image('bg-shop', 'bg-shop.png');
-        this.load.image('bg-grass', 'bg-grass.png');
-        this.load.image('bg-desert', 'bg-desert.png');
-        this.load.image('bg-tundra', 'bg-tundra.png');
-        this.load.image('bg-cave', 'bg-cave.png');
-
-        // Sound Effects
-        this.load.audio('attack-immune', 'sfx/attack-immune.wav');
-        this.load.audio('attack-weak', 'sfx/attack-weak.wav');
-        this.load.audio('attack-neutral', 'sfx/attack-neutral.wav');
-        this.load.audio('attack-effective', 'sfx/attack-effective.wav');
-        this.load.audio('attack-supereffective', 'sfx/attack-supereffective.wav');
-        this.load.audio('battle-select-enemy', 'sfx/battle-select-enemy.wav');
-        this.load.audio('battle-select-enemy-attack', 'sfx/battle-select-enemy-attack.wav');
-        this.load.audio('battle-win', 'sfx/battle-win.wav');
-        this.load.audio('battle-lose', 'sfx/battle-lose.wav');
-        this.load.audio('battle-ice-attack', 'sfx/battle-ice-attack.wav');
-        this.load.audio('battle-phys-attack', 'sfx/battle-phys-attack.wav');
-        this.load.audio('battle-fire-attack', 'sfx/battle-fire-attack.wav');
-        this.load.audio('battle-def', 'sfx/battle-def.wav');
-        this.load.audio('prebattle-move-spirit', 'sfx/prebattle-move-spirit.wav');
-        this.load.audio('button-press-1', 'sfx/button-press-1.wav');
-        this.load.audio('upgrade-success', 'sfx/upgrade-success.wav');
-
-        // Music
-        this.load.audio('menu-music', 'music/menu.wav');
-        this.load.audio('boss-battle-music', 'music/boss-battle-music.wav');
-
-        this.load.plugin('rexdragplugin', 'https://raw.githubusercontent.com/rexrainbow/phaser3-rex-notes/master/dist/rexdragplugin.min.js', true);
-        this.load.plugin('rexroundrectangleplugin', 'https://raw.githubusercontent.com/rexrainbow/phaser3-rex-notes/master/dist/rexroundrectangleplugin.min.js', true);
-    }
-
     create() {
         // Add and launch dungeon background scene first (shared across hub scenes)
         if (!this.scene.get('DungeonScene')) {
@@ -142,9 +43,6 @@ export class MainMenu extends Phaser.Scene {
         if (dungeonScene && !dungeonScene.scene.isActive()) {
             this.scene.launch('DungeonScene');
         }
-
-        createSpiritAnimations(this);
-        createEnemyAnimations(this);
 
         // Initialize UI immediately since we're coming from BootScene with initialized API
         // Scene status is CREATING during create(), so we bypass onStateChange's status check
